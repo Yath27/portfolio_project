3e2ae54245b314a32f15b3af86b1844212055656
Yath27 <106150697+Yath27@users.noreply.github.com>
2024-08-20 19:22:13 +0530

    Update covid data project.sql

--- covid data project.sql
+++ covid data project.sql
@@ -1,50 +1,38 @@
-Select *
-from portfolio_project..covid_data
-order by 2,3
-
--- finding the death rate on each day
-
-select continent, location, date, population, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_rate
-from portfolio_project..covid_data
+--[Q1] Finding the death rate on each day
+Select continent, location, date, population, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_rate
+from covid_data
 order by 1,2
 
 
--- percentage of population infected
-
-select  continent, location, date, population, total_cases, (total_cases/population)*100 as population_infected
-from portfolio_project..covid_data
+--[Q2] Percentage of population infected
+Select  continent, location, date, population, total_cases, (total_cases/population)*100 as population_infected
+from covid_data
 order by 1,2
 
 
-
---countries with highest death count
-
-select location, max(cast(total_deaths AS int)) as total_death_count
-from portfolio_project..covid_data
+--[Q3] Countries with highest death count
+Select location, max(cast(total_deaths AS int)) as total_death_count
+from covid_data
 group by location
 order by total_death_count desc
 
 
-
--- highest cases of different countries on a single day
-
-select location, max(new_cases) as most_cases
-from portfolio_project..covid_data
+--[Q4] Highest cases of different countries on a single day
+Select location, max(new_cases) as most_cases
+from covid_data
 group by location
 order by most_cases desc
 
 
--- global death percentage
-
-select sum(new_cases) as total_cases_globally, sum(cast(new_deaths as int)) as total_deaths_globally, 
+--[Q5] Global death percentage
+Select sum(new_cases) as total_cases_globally, sum(cast(new_deaths as int)) as total_deaths_globally, 
 (sum(cast(new_deaths as int))/sum(new_cases))*100 as global_death_percentage
-from portfolio_project..covid_data
+from covid_data
 
 
--- total deaths across continents
-
+--[Q6] Total deaths across continents
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From Portfolio_Project..Covid_data
+From Covid_data
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
